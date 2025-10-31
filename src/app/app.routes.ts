0fb07766699267d3c6400d47c497bf0630b053eb
HenningOtte <HenningOtte91@gmail.com>
2025-10-31 19:08:41 +0100

    feat(routes): add main-content and imprint components to app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Maincontent } from './main-content/maincontent';
+import { Imprint } from './imprint/imprint';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Maincontent },
+    { path: 'imprint', component: Imprint }
+];
